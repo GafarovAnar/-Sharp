e8a2f21dc22efaa0ccbfbdb11bbceb626c3adc46
GafarovAnar <anar-50@mail.ru>
2022-11-12 00:20:28 +0300

    Find square

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти квадрат целого числа
+// 1.Ввод целого числа
+// 2.Нахождение квадрата целого числа 
+Console.WriteLine("Введите целое число для нахождения его квадарата : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number*number;
+Console.WriteLine($"Квадрта числа {number} = {square}");
