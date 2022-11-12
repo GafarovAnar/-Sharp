1c61fe86061c970109729012d1d1ad8c10333ba7
GafarovAnar <anar-50@mail.ru>
2022-11-12 14:05:21 +0300

    Find max number of three"

---
+++ Task04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+// 1. Принимает на ввод 3 числа.
+// 2.Находит максимальное.
+Console.WriteLine("Введите первое число для сравнения : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число для сравнения : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число для сравнения : ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (num1 > num2)
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+if (max < num3)
+{
+    max = num3;
+}
+Console.WriteLine($"Максимальное число - {max}");
