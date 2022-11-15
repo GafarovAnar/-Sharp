e5e6c7b41c07f4004bbd8cbdc3bc60035bf16b79
GafarovAnar <anar-50@mail.ru>
2022-11-15 22:41:26 +0300

    Output a random number and find maximum digit the number

---
+++ Task09/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа
+// 1.Выводит случайное число от 10 до 99.
+// 2.Показывает наибольшую цифру числа.
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случаное число из отрезка от 10 до 99 --> {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit > secondDigit) Console.WriteLine($"Наибольшее число --> {firstDigit}");
+else Console.WriteLine($"Наибольшее число --> {secondDigit}");
