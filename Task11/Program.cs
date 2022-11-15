5365810f70af792d1f36d3eda123611a8bf8a361
GafarovAnar <anar-50@mail.ru>
2022-11-15 23:38:44 +0300

    Count sum the first digit and third digit

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 1.Выводит число от 100 до 999.
+// 2.Удаляет вторую цифру.
+// 456 -> 46
+int CountDigit(int num)
+{
+    int firstDigit=num/100;
+    int lastDigit=num%10;
+    int count=firstDigit*10+lastDigit;
+    return count;
+}
+int number= new Random().Next(100,1000);
+Console.WriteLine($"Случайное число от 100 до 999 --> {number}");
+Console.WriteLine(CountDigit(number));
