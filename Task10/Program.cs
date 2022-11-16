bd335d5c7b6321d16d9be25c8829a53653710e50
GafarovAnar <anar-50@mail.ru>
2022-11-16 17:06:46 +0300

    Tрhre digit number, shown the second digit

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿bool CheckNumber(int num)
+{
+    return (num < 1000) && (num > 99);
+}
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(CheckNumber(number) ? $"Третья цифра числа {number} --> {(number/10)%10}" : "Число не трехзначное");
