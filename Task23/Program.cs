d03313d1b77d742cc95297f08c282a59827072cd
GafarovAnar <anar-50@mail.ru>
2022-11-22 21:52:29 +0300

    Cubing a number

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 1.Принимает число.
+// 2.Выдает таблицу кубов.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+void Coub(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i}|{i * i * i}");
+    }
+}
+Coub(number);
