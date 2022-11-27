aad1ecb33f8b4aa33d1db2dc44ef56c4d174474f
GafarovAnar <anar-50@mail.ru>
2022-11-27 22:50:23 +0300

    Sum of numbers

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1.На вход принимает число.
+// 2.Выдает сумму чисел.
+System.Console.WriteLine("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Sum(int num)
+{
+    int count = 0;
+    if (num < 0) num = -1 * num;
+    while (num > 0)
+    {
+        count = count + num % 10;
+        num /= 10;
+    }
+    return count;
+
+}
+int sum = Sum(number);
+Console.WriteLine($"Sum of numbers {number} -> {sum}");
+
+
