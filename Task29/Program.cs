3267031f4310d8564e4f01dd8dfc494fdb2b2166
GafarovAnar <anar-50@mail.ru>
2022-11-27 23:07:42 +0300

    Create and print an array

---
+++ Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1.Задает массив.
+// 2.Выводит массив на экран.
+System.Console.WriteLine("Enter the size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+void Array(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr[i] = rnd.Next(1, 100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+Array(array);
+PrintArray(array);
