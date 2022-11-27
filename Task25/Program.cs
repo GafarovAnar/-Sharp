ca50541a681436043d29bb3e6e78d053d0981fa3
GafarovAnar <anar-50@mail.ru>
2022-11-27 22:49:40 +0300

    Degree

---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// 1.Принимает два числа А и Б.
+// 2. Чисоло А возводится в натуральную степень Б.
+Console.WriteLine("Enter number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int Degree(int x, int y)
+{
+    int count = 1;
+    int deg = 0;
+    while (deg < y)
+    {
+        count = count * x;
+        deg++;
+    }
+    return count;
+
+}
+if (b > 0)
+{
+    int ab = Degree(a, b);
+    Console.WriteLine($"Number {a} is a degree {b} = {ab}");
+}
+else Console.WriteLine("Введите положительное число B.");
+
