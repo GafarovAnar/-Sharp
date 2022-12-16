69f871b8c257a907f89a7a0d14e0eaa70e8ca275
GafarovAnar <anar-50@mail.ru>
2022-12-16 23:49:17 +0300

    count positive digits

---
+++ Task41/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива :");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]} ");
+        else Console.WriteLine($"{arr[i]}");
+    }
+}
+int CountPosotiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] newArray = FillArray(sizeArray);
+PrintArray(newArray);
+int countPositiveNumber = CountPosotiveNumber(newArray);
+Console.WriteLine(countPositiveNumber);
+
