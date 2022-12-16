07cb2626c67b1f214bf059a6064a0695b7886e39
GafarovAnar <anar-50@mail.ru>
2022-12-17 00:42:41 +0300

    Find x and y

---
+++ TAsk43/Program.cs
@@ -0,0 +1,24 @@
+﻿double[] Array(double meaningB1, double meaningK1, double meaningB2, double meaningK2)
+{
+    double[] array = new double[2];
+    double x = (meaningB2 - meaningB1) / (meaningK1 - meaningK2);
+    double y = meaningK1 * x + meaningB1;
+    array[0] = x;
+    array[1] = y;
+    return array;
+}
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]} ");
+        else Console.WriteLine($"{arr[i]}");
+    }
+}
+Console.WriteLine("Введите четыре числа для определения точек пересечения прямых");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double[] newArray = Array(b1, k1, b2, k2);
+PrintArray(newArray);
